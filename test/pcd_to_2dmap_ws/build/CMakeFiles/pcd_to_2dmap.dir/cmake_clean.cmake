f1bce32aaefd14d2871f1d0dc4e13122b331b6d4
khs <gkstjd543@koreatech.ac.kr>
2025-03-11 17:10:51 +0900

    commit

---
+++ test/pcd_to_2dmap_ws/build/CMakeFiles/pcd_to_2dmap.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pcd_to_2dmap.dir/src/main.cpp.o"
+  "CMakeFiles/pcd_to_2dmap.dir/src/main.cpp.o.d"
+  "pcd_to_2dmap"
+  "pcd_to_2dmap.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pcd_to_2dmap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ test/pcd_to_2dmap_ws/build/CMakeFiles/pcd_to_2dmap.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for pcd_to_2dmap.
+# This may be replaced when dependencies are built.
